e9b0bea1dd704009a5a2bde9ff632182b1c2fc7e
HIJIRI814 <hijiri8814@gmail.com>
2022-09-10 12:37:18 +0900

    ページ実装

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,104 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="box">
+      <h1 style="grid-area: title">見てくれてアリガトー！</h1>
+      <div style="grid-area: text">
+        <p>
+          わざわざQRコードから来てくれてありがとうございます。<br />
+          Vol.1, Vol.2
+          両方手に入れたかな？持ってるよ！って人はほんとありがとう！！<br />
+          まだ持ってない方もぜひもう片方のアルバムもチェックしてみてね！！<br />
+          委託もあるぞ！<br />
+          続報はYouTube,ニコニコ動画の動画説明欄、IbEthics公式・がむのツイッターをチェック！！
+        </p>
+        <p>
+          <a
+            href="https://www.melonbooks.co.jp/detail/detail.php?product_id=1628655"
+            >Vol.1委託はこちら</a
+          ><br />
+          <a
+            href="https://www.melonbooks.co.jp/detail/detail.php?product_id=1628656"
+            >Vo.2委託はこちら</a
+          ><br />
+          <a
+            href="https://www.melonbooks.co.jp/detail/detail.php?product_id=1634085"
+            >両方持ってないのになぜかこのページに来れた人はこちら</a
+          >
+        </p>
+      </div>
+      <img src="/img/mary.png" alt="" class="mary" style="grid-area: char2" />
+      <img src="/img/renko.png" alt="" class="renko" style="grid-area: char1" />
+      <div class="video" style="grid-area: movie">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/UZLR1ynbHwc"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+h1 {
+  margin: 0;
+  text-align: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+p {
+  line-height: 1.5;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+
+.box {
+  width: 80%;
+  margin: 32px auto;
+  display: grid;
+  gap: 24px 12px;
+  grid-template:
+    "title title title" auto
+    "char1 text char2" auto
+    "movie movie movie" auto / 250px 1fr 250px;
+}
+
+.video {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 56.25%;
+}
+
+.video iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+img {
+  width: 100%;
+}
+
+@media (max-width: 820px) {
+  .box {
+    grid-template:
+      "title title" auto
+      "char1 char2" auto
+      "text  text " auto
+      "movie movie" auto / 1fr 1fr;
+  }
 }
 </style>
